4088dd2cb4bb54574c0873de19b0e64d8e3ecf08
Alfanor <alfanor@hotmail.fr>
2018-02-11 16:16:11 +0100

    Transfert des textes de l'accueil dans les fichiers langue.

--- Vues/Langage/FR/accueil.php
+++ Vues/Langage/FR/accueil.php
@@ -1,2 +1,14 @@
 <?php
+$_LANGUAGE['FR']['CONNEXION_FORM'] = 'S\'identifier';
+$_LANGUAGE['FR']['CONNEXION'] = 'Hey ! Vous êtes qui vous ? Vous n\'irez pas plus loin tant que vous ne vous serez pas identifié mon gars !';
+$_LANGUAGE['FR']['CONNEXION_ERROR'] = 'Vous croyez vraiment que vous allez passer en me donnant un nom au hasard ? Vous n\'avez pas intérêt à continuer comme ça mon gars !';
+
+$_LANGUAGE['FR']['ACCUEIL_MESSAGE'] = 'Il serait temps de vous mettre à travailler ! Pour le moment vous êtes notre leader, mais si vous continuez à dormir je peux vous dire que vous allez vite finir dans le fleuve ! Voici un rapport de la situation actuelle :';
+
+$_LANGUAGE['FR']['ACCUEIL_ERROR'] = 'Hum... En fait votre temple est introuvable ! Vous devriez penser à prier nos dieux pour le retrouver !';
+
+$_LANGUAGE['FR']['RESUME_TEMPLE_NAME'] = 'Nom de votre temple :';
+$_LANGUAGE['FR']['RESUME_TEMPLE_LEVEL'] = 'Niveau de votre temple :';
+$_LANGUAGE['FR']['RESUME_RESSOURCES'] = 'Ressources que vous possédez :';
+$_LANGUAGE['FR']['RESUME_TEMPLE_RESSOURCES'] = 'Ressources affectées au temple :';
 ?>
--- Vues/Template/accueil.php
+++ Vues/Template/accueil.php
@@ -2,16 +2,16 @@
 if(!isset($_SESSION['id']))
 {
     if(isset($_GET['erreur']) && ($_GET['erreur'] == 1))
-        echo '<p>Vous croyez vraiment que vous allez passer en me donnant un nom au hasard ? Vous n\'avez pas intérêt à continuer comme ça mon gars !</p>';
+        echo '<p>' . $_LANGUAGE['FR']['CONNEXION_ERROR'] . '</p>';
 
     else
-        echo '<p>Hey ! Vous êtes qui vous ? Vous n\'irez pas plus loin tant que vous ne vous serez pas identifié mon gars !</p>';
+        echo '<p>' . $_LANGUAGE['FR']['CONNEXION'] . '</p>';
     ?>
 
     <form method="post" action="?p=connexion"> 
         <strong>Nom</strong><input type="text" name="login" maxlength="20" />
         <strong>Passe</strong><input type="password" name="password" />
-        <input type="submit" value="S'identifier" />
+        <input type="submit" value="<?php echo $_LANGUAGE['FR']['CONNEXION_FORM']; ?>" />
     </form> 
 
     <?php    
@@ -19,26 +19,26 @@ if(!isset($_SESSION['id']))
 
 else
 {    
-    echo '<p>Il serait temps de vous mettre à travailler ! Pour le moment vous êtes notre leader, mais si vous continuez à dormir je peux vous dire que vous allez vite finir dans le fleuve ! Voici un rapport de la situation actuelle :</p>';
+    echo '<p>' . $_LANGUAGE['FR']['ACCUEIL_MESSAGE'] . '</p>';
 
     if($compte_erreur)
-        echo '<p>Hum... En fait votre temple est introuvable ! Vous devriez penser à prier nos dieux pour le retrouver !</p>';
+        echo '<p>' . $_LANGUAGE['FR']['ACCUEIL_ERROR'] . '</p>';
 
     else
     {
         echo '  <ul>
-                    <li>Nom de votre temple : ' . $membre->getTemple()->GetName() . '</li>
-                    <li>Niveau de votre temple : ' . $membre->getTemple()->getLevel() . '</li>
-                    <li>Ressources que vous possédez : <ul>';
+                    <li>' . $_LANGUAGE['FR']['RESUME_TEMPLE_NAME'] . ' ' . $membre->getTemple()->GetName() . '</li>
+                    <li>' . $_LANGUAGE['FR']['RESUME_TEMPLE_LEVEL'] . ' ' . $membre->getTemple()->getLevel() . '</li>
+                    <li>' . $_LANGUAGE['FR']['RESUME_RESSOURCES'] . '<ul>';
 
-        foreach($membre->getInventory()->getRessources() as $id => $quantite)
-            echo '<li>' . $_LANGUAGE['FR']['RESSOURCE_' . $id] . ' : ' . $quantite . '</li>';
+        foreach($membre->getInventory()->getRessources() as $id => $amount)
+            echo '<li>' . $_LANGUAGE['FR']['RESSOURCE_' . $id] . ' : ' . $amount . '</li>';
 
-        echo '</ul>
-                    <li>Ressources affectées au temple : <ul>';
+        echo '  </ul>
+                    <li>' . $_LANGUAGE['FR']['RESUME_TEMPLE_RESSOURCES'] . '<ul>';
 
-        foreach($membre->getTemple()->getInventory()->getRessources() as $id => $quantite)
-            echo '<li>' . $_LANGUAGE['FR']['RESSOURCE_' . $id] . ' : ' . $quantite . '</li>';
+        foreach($membre->getTemple()->getInventory()->getRessources() as $id => $amount)
+            echo '<li>' . $_LANGUAGE['FR']['RESSOURCE_' . $id] . ' : ' . $amount . '</li>';
 
         echo '</ul>  </ul>';
     }
