e5a161998038d5435ada6fb7523d1cae4e3483bb
VincHawthorne <36601032+VincHawthorne@users.noreply.github.com>
2018-03-02 19:26:49 +0100

    Amelioration de la gestion des temples
    Le joueur peut, pour chaque temple en sa possession :
    - voir les ressources consommées jusque là
    - voir les ressources actuellement prêtes à être utilisées

--- Controleurs/temple.php
+++ Controleurs/temple.php
@@ -24,11 +24,31 @@ if(isset($_SESSION['id']))
     // We want to know the informations of all member's temples
     foreach($_SESSION['data']->getVillages() as $village)
     {
+		$villageTemple = $village->getTemple();
+		
 		// If there is a temple in this village
-		if(isset($village->getTemple()))
+		if(!empty($villageTemple))
 		{
+
+			$_SQL = SQL::getInstance();
+
+			$req = 'SELECT temple_id, level_id, inventory_id FROM temple_running_level WHERE temple_id = :id';
+
+			$rep = $_SQL->prepare($req);
+
+			$rep->execute(array(':id' => $village->getTemple()->getId()));
+
+			$resultat = $rep->fetchAll();
+			
+			if(count($resultat) == 1)
+			{
+				$runningLvlInventory = new Inventory($resultat[0]['inventory_id']);
+				$runningLvlInventory->loadFromId();
+			}
+			
 			$temples[$village->getName()]['village'] = $village;
-			$temples[$village->getName()]['temple'] = $village->getTemple();
+			$temples[$village->getName()]['temple'] = $village->getTemple();			
+			$temples[$village->getName()]['inventory'] = $runningLvlInventory;
 		}			
     }		
 
--- Vues/Template/temple.php
+++ Vues/Template/temple.php
@@ -4,11 +4,43 @@ foreach($temples as $nomVivi => $contenu)
 {
 	foreach($contenu as $value => $donnee)
 	{
-		if($value == "temple")
+		switch($value)
 		{
-			echo sprintf($_LANGUAGE['FR']['TEMPLE_ACTUELS'],$donnee->getLevel(),$nomVivi);
-		}	
+			case 'village':
+				$currentVillage = $donnee;
+				break;
+			
+			case 'temple':
+				$currentTemple = $donnee;
+				break;
+			
+			case 'inventory':
+				$currentInventory = $donnee;
+				break;
+		}
 	}
+	
+	echo sprintf($_LANGUAGE['FR']['TEMPLE_ACTUELS'],$currentTemple->getLevel(),$currentVillage->getName());
+	
+	echo $_LANGUAGE['FR']['TEMPLE_RESSCONSOMMEES'];
+	
+    echo '<ul>';
+
+    foreach($currentInventory->getRessources() as $id => $amount)
+        echo '<li>' . $_LANGUAGE['FR']['RESSOURCE_' . $id] . ' : ' . $amount . '</li>';
+
+    echo '</ul>';	
+	
+	echo $_LANGUAGE['FR']['TEMPLE_RESSDISPO'];
+	
+    echo '<ul>';
+
+    foreach($currentTemple->getInventory()->getRessources() as $id => $amount)
+        echo '<li>' . $_LANGUAGE['FR']['RESSOURCE_' . $id] . ' : ' . $amount . '</li>';
+
+    echo '</ul>';	
+	
+	
 	
 	echo "<br/><br/>";
 }
